5a17a31ae4da02df542606aafdc67f362242e4cf
dorismili <106894567+dorismili@users.noreply.github.com>
2022-06-04 20:59:53 -0500

    cosas de Doris

---
+++ JS variable.js
@@ -0,0 +1,14 @@
+// Doris Lange
+let nombre = "Doris Lange";
+let edad = "15";
+let altura = "1.65";
+let comida = "espagueti con pollo";
+let color =  "rojo";
+let numero = (25 + 25);
+
+document.write (nombre) ;
+document.write (edad);
+document.write (altura) ;
+document.write (comida) ;
+document.write (color) ;
+document.write (25 +25 );
